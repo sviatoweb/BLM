aab4ba25c69d22f050108b030489ad56181a5fd2
Sviatoslav Stehnii <81622077+sviatoweb@users.noreply.github.com>
2023-05-30 12:48:22 +0300

    Update fero.py

--- fero.py
+++ fero.py
@@ -6,21 +6,15 @@ class Fero_world:
     def __init__(self, size) -> None:
         self.grid = [[{'black': 0.0, 'white': 0.0, 'yellow': 0.0, 'orange': 0.0} for _ in range(size)] for _ in range(size)]
 
-    def distance_to_respawn(self, cords, world):
-        size = len(world.grid)
-        return math.dist(cords, (int(size//2),int(size//2)))
-
     def add_phero(self, tip, world):
-        k = 1
-        dist = self.distance_to_respawn(tip.cords, world)
         if tip.race == Races.BLACK:
-            self.grid[tip.cords[0]][tip.cords[1]]['black'] += k*tip.energy/100
+            self.grid[tip.cords[0]][tip.cords[1]]['black'] += tip.energy/100
         if tip.race == Races.WHITE:
-            self.grid[tip.cords[0]][tip.cords[1]]['white'] += k*tip.energy/100
+            self.grid[tip.cords[0]][tip.cords[1]]['white'] += tip.energy/100
         if tip.race == Races.ORANGE:
-            self.grid[tip.cords[0]][tip.cords[1]]['orange'] += k*tip.energy/100
+            self.grid[tip.cords[0]][tip.cords[1]]['orange'] += tip.energy/100
         if tip.race == Races.YELLOW:
-            self.grid[tip.cords[0]][tip.cords[1]]['yellow'] += k*tip.energy/100
+            self.grid[tip.cords[0]][tip.cords[1]]['yellow'] += tip.energy/100
 
     def clear_phero(self):
         for i in range(len(self.grid)):
